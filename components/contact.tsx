08a2c70fd533aef42d29edee0e755356824083f2
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-30 23:35:48 +0600

    Update contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,79 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { ScrollAnimate } from "./motion/scroll-animation";
 import { Send } from "lucide-react";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { ContactFormData, contactSchema } from "@/lib/validators/contactSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { sendEmail } from "@/lib/actions/sendEmail";
+import { toast } from "sonner";
 
 export function Contact() {
+	const form = useForm<ContactFormData>({
+		resolver: zodResolver(contactSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			subject: "",
+			message: "",
+		},
+	});
+
+	const onSubmit = async (data: ContactFormData) => {
+		try {
+			const formData = new FormData();
+			formData.append("name", data.name);
+			formData.append("email", data.email);
+			formData.append("subject", data.subject);
+			formData.append("message", data.message);
+
+			const res = await sendEmail(formData);
+
+			if (res.success) {
+				toast.success("Email sent successfully!", {
+					description: "Thanks for reaching out!",
+					action: {
+						label: "Close",
+						onClick: () => {
+							toast.dismiss();
+						},
+					},
+				});
+			} else {
+				toast.error("Something went wrong!", {
+					description: "Please try again later.",
+					action: {
+						label: "Close",
+						onClick: () => {
+							toast.dismiss();
+						},
+					},
+				});
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error("Something went wrong!", {
+				description: "Please try again later.",
+				action: {
+					label: "Close",
+					onClick: () => {
+						toast.dismiss();
+					},
+				},
+			});
+		}
+	};
+
 	return (
 		<section id="contact" className="py-12 px-4 max-w-2xl mx-auto">
 			<ScrollAnimate
@@ -22,38 +90,92 @@ export function Contact() {
 			</ScrollAnimate>
 
 			<ScrollAnimate variant="fadeUp" delay={0.3}>
-				<form className="space-y-6">
-					<div className="grid md:grid-cols-2 gap-6">
-						<div className="space-y-2">
-							<Input placeholder="Name" className="h-12" />
-						</div>
-						<div className="space-y-2">
-							<Input
-								placeholder="Email"
-								type="email"
-								className="h-12"
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-6"
+					>
+						<div className="grid md:grid-cols-2 gap-6">
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												placeholder="Name"
+												className="h-12"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												type="email"
+												placeholder="Email"
+												className="h-12"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
 							/>
 						</div>
-					</div>
-					<div className="space-y-2">
-						<Input placeholder="Subject" className="h-12" />
-					</div>
-					<div className="space-y-2">
-						<Textarea
-							placeholder="Message"
-							className="min-h-[150px]"
+
+						<FormField
+							control={form.control}
+							name="subject"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input
+											placeholder="Subject"
+											className="h-12"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
 						/>
-					</div>
-					<ScrollAnimate variant="fadeUp" delay={0.4}>
-						<Button
-							type="submit"
-							size="lg"
-							className="w-full gap-2 flex items-center"
-						>
-							Send Message <Send className="w-4 h-4" />
-						</Button>
-					</ScrollAnimate>
-				</form>
+
+						<FormField
+							control={form.control}
+							name="message"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Textarea
+											placeholder="Message"
+											className="min-h-[150px]"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<ScrollAnimate variant="fadeUp" delay={0.4}>
+							<Button
+								type="submit"
+								size="lg"
+								className="w-full gap-2 flex items-center"
+							>
+								Send Message <Send className="w-4 h-4" />
+							</Button>
+						</ScrollAnimate>
+					</form>
+				</Form>
 			</ScrollAnimate>
 		</section>
 	);
