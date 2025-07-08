75d95018b17ef1bca9c7792ba7c8776faf79d681
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-07-09 00:32:07 +0600

    update mobile navbar style

--- components/header.tsx
+++ components/header.tsx
@@ -42,41 +42,13 @@ export default function Header() {
 			<ScrollAnimate
 				variant="fadeDown"
 				ease="easeIn"
-				className={`fixed top-0 left-0 right-0 bg-background z-50 border-b transition duration-300 ${
-					isScrolled ? "shadow" : "border-transparent"
-				}`}
-			>
+				className={`fixed top-0 left-0 right-0 bg-background z-50 border-b transition duration-300 ${isScrolled ? "border-muted" : "border-transparent"}`}>
 				<nav className="flex items-center justify-between max-w-6xl mx-auto min-h-16 px-2 sm:px-4">
 					<div className="block md:hidden">
-						<Button
-							className={clsx(open && "bg-secondary/80")}
-							onClick={toggleMenu}
-							variant="ghost"
-							size="iconLg"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="50"
-								height="50"
-								fill="none"
-								viewBox="0 0 48 48"
-								stroke="currentColor"
-								strokeWidth="6"
-							>
-								<line
-									x1="4"
-									y1="14"
-									x2="44"
-									y2="14"
-									strokeLinecap="round"
-								/>
-								<line
-									x1="4"
-									y1="34"
-									x2="44"
-									y2="34"
-									strokeLinecap="round"
-								/>
+						<Button className={clsx(open && "bg-secondary/80")} onClick={toggleMenu} variant="ghost" size="iconLg">
+							<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none" viewBox="0 0 48 48" stroke="currentColor" strokeWidth="6">
+								<line x1="4" y1="14" x2="44" y2="14" strokeLinecap="round" />
+								<line x1="4" y1="34" x2="44" y2="34" strokeLinecap="round" />
 							</svg>
 						</Button>
 					</div>
@@ -85,22 +57,9 @@ export default function Header() {
 						Hussain
 					</Link>
 
-					<div
-						className={`${
-							open ? "" : "-translate-x-full"
-						} transition transform duration-300 md:hidden fixed top-16 left-0 right-0 bg-background border-t flex flex-col h-full`}
-					>
+					<div className={`${open ? "" : "-translate-x-full"} transition transform duration-300 md:hidden fixed top-16 left-0 right-0 bg-background flex flex-col h-full`}>
 						{navLinks.map(({ href, content }) => (
-							<Link
-								className={clsx(
-									"mobile-nav-link",
-									pathname === href &&
-										"text-neutral-950 dark:text-neutral-50"
-								)}
-								href={href}
-								key={href}
-								onClick={handleClose}
-							>
+							<Link className={clsx("mobile-nav-link", pathname === href && "text-neutral-950 dark:text-neutral-50")} href={href} key={href} onClick={handleClose}>
 								{content}
 							</Link>
 						))}
@@ -108,15 +67,7 @@ export default function Header() {
 
 					<div className="space-x-8 hidden md:block">
 						{navLinks.map(({ href, content }) => (
-							<Link
-								className={clsx(
-									"nav-link",
-									pathname === href &&
-										"text-neutral-950 dark:text-neutral-50"
-								)}
-								href={href}
-								key={href}
-							>
+							<Link className={clsx("nav-link", pathname === href && "text-neutral-950 dark:text-neutral-50")} href={href} key={href}>
 								{content}
 							</Link>
 						))}
